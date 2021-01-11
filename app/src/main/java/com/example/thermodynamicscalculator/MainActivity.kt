7679ee3b8cdd36287f2c72675d3336d44fe36339
ke7omc <ke7omc@gmail.com>
2021-01-11 09:02:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/thermodynamicscalculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.thermodynamicscalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.submit_button)
+        button.setOnClickListener{
+        val editName
+        }
+    }
+}
